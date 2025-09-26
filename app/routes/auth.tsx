23e920f66da9dcc0a1e48f1bab17b37c368ccb7e
Iye-Isaac <iyeoluwa.isaac@gmail.com>
2025-09-26 14:51:08 +0100

    second commit

---
+++ app/routes/auth.tsx
@@ -0,0 +1,15 @@
+export const meta = () => ([
+    { title: 'Resumind | Auth' },
+    { name: 'description', content: 'Log into your account' },
+])
+const Auth = () => {
+    return (
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col"></section>
+            </div>
+        </main>
+    )
+}
+
+export default Auth
